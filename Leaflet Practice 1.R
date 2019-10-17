fd253cace692f4d24e1189981a42edba258ea140
kimj20 <kimj20@mail.wlu.edu>
2019-10-17 12:02:30 -0400

    This is a Leaflet Practice!

---
+++ Leaflet Practice 1.R
@@ -0,0 +1,22 @@
+library(leaflet)
+leaflet()
+leaflet() %>% addTiles()
+leaflet() %>% setView(lng = -79.442778, lat = 37.783889, zoom = 12) %>% addTiles()
+leaflet() %>% 
+  setView(lng = -79.442778, lat = 37.783889, zoom = 8) %>% 
+  addProviderTiles(provider = providers$NASAGIBS.ViirsEarthAtNight2012)
+
+#good for heat maps and colored maps 
+leaflet() %>% 
+  setView(lng = -79.442778, lat = 37.783889, zoom = 12) %>% 
+  addProviderTiles(providers$CartoDB.Positron)
+#web map service:
+leaflet() %>% 
+  addTiles() %>% 
+  setView(lng = -79.442778, lat = 37.783889, zoom = 8) %>%
+  addWMSTiles(
+    "http://mesonet.agron.iastate.edu/cgi-bin/wms/nexrad/n0r.cgi",
+    layers = "nexrad-n0r-900913",
+    options = WMSTileOptions(format = "image/png", transparent = TRUE),
+    attribution = "Weather data © 2012 IEM Nexrad"
+  )
